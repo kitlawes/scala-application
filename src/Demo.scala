8c52907da9be573e1ece4870c2f7c53359bb019d
kitlawes <kitlawes@hotmail.com>
2018-03-29 12:01:50 +0100

    Update Demo.scala to demonstrate infinite loops

--- src/Demo.scala
+++ src/Demo.scala
@@ -1,12 +1,10 @@
 object Demo {
   def main(args: Array[String]) {
-    var x = 30;
-    var y = 10;
+    var a = 10;
 
-    if( x == 30 ){
-      if( y == 10 ){
-        println("X = 30 and Y = 10");
-      }
+    // An infinite loop.
+    while( true ){
+      println( "Value of a: " + a );
     }
   }
 }
