30fd99df27e89217439fdaf54a929f6b4e9efe82
kitlawes <kitlawes@hotmail.com>
2018-03-29 11:04:06 +0100

    Update Demo.scala to demonstrate classes and class inheritance

--- src/Demo.scala
+++ src/Demo.scala
@@ -1,11 +1,36 @@
+import java.io._
+
+class Point(val xc: Int, val yc: Int) {
+  var x: Int = xc
+  var y: Int = yc
+
+  def move(dx: Int, dy: Int) {
+    x = x + dx
+    y = y + dy
+    println ("Point x location : " + x);
+    println ("Point y location : " + y);
+  }
+}
+
+class Location(override val xc: Int, override val yc: Int,
+               val zc :Int) extends Point(xc, yc){
+  var z: Int = zc
+
+  def move(dx: Int, dy: Int, dz: Int) {
+    x = x + dx
+    y = y + dy
+    z = z + dz
+    println ("Point x location : " + x);
+    println ("Point y location : " + y);
+    println ("Point z location : " + z);
+  }
+}
+
 object Demo {
   def main(args: Array[String]) {
-    var myVar :Int = 10;
-    val myVal :String = "Hello Scala with datatype declaration.";
-    var myVar1 = 20;
-    val myVal1 = "Hello Scala new without datatype declaration.";
+    val loc = new Location(10, 20, 15);
 
-    println(myVar); println(myVal); println(myVar1);
-    println(myVal1);
+    // Move to a new location
+    loc.move(10, 10, 5);
   }
 }
