e3e62ac746044f3cbcf8a1c22302eeac9131646b
kitlawes <kitlawes@hotmail.com>
2018-03-29 12:38:48 +0100

    Update Demo.scala to demonstrate pattern matching using case classes

--- src/Demo.scala
+++ src/Demo.scala
@@ -1,23 +1,17 @@
-trait Equal {
-  def isEqual(x: Any): Boolean
-  def isNotEqual(x: Any): Boolean = !isEqual(x)
-}
-
-class Point(xc: Int, yc: Int) extends Equal {
-  var x: Int = xc
-  var y: Int = yc
-
-  def isEqual(obj: Any) = obj.isInstanceOf[Point] && obj.asInstanceOf[Point].x == y
-}
-
 object Demo {
   def main(args: Array[String]) {
-    val p1 = new Point(2, 3)
-    val p2 = new Point(2, 4)
-    val p3 = new Point(3, 3)
+    val alice = new Person("Alice", 25)
+    val bob = new Person("Bob", 32)
+    val charlie = new Person("Charlie", 32)
 
-    println(p1.isNotEqual(p2))
-    println(p1.isNotEqual(p3))
-    println(p1.isNotEqual(2))
+    for (person <- List(alice, bob, charlie)) {
+      person match {
+        case Person("Alice", 25) => println("Hi Alice!")
+        case Person("Bob", 32) => println("Hi Bob!")
+        case Person(name, age) => println(
+          "Age: " + age + " year, name: " + name + "?")
+      }
+    }
   }
+  case class Person(name: String, age: Int)
 }
