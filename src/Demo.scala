0eec34b5ab7750f43b36244950f3e6108a5861b6
kitlawes <kitlawes@hotmail.com>
2018-03-29 11:40:58 +0100

    Update Demo.scala to demonstrate singleton objects

--- src/Demo.scala
+++ src/Demo.scala
@@ -1,7 +1,23 @@
-import Run._
+import java.io._
+
+class Point(val xc: Int, val yc: Int) {
+  var x: Int = xc
+  var y: Int = yc
+
+  def move(dx: Int, dy: Int) {
+    x = x + dx
+    y = y + dy
+  }
+}
 
 object Demo {
   def main(args: Array[String]) {
-    4 times println("hello")
+    val point = new Point(10, 20)
+    printPoint
+
+    def printPoint{
+      println ("Point x location : " + point.x);
+      println ("Point y location : " + point.y);
+    }
   }
 }
