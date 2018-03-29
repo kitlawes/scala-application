621cea2f705b5cbbbfb40547f50b7668b9d6ee76
kitlawes <kitlawes@hotmail.com>
2018-03-29 12:34:57 +0100

    Update Demo.scala to demonstrate traits

--- src/Demo.scala
+++ src/Demo.scala
@@ -1,18 +1,23 @@
-import Array._
+trait Equal {
+  def isEqual(x: Any): Boolean
+  def isNotEqual(x: Any): Boolean = !isEqual(x)
+}
+
+class Point(xc: Int, yc: Int) extends Equal {
+  var x: Int = xc
+  var y: Int = yc
+
+  def isEqual(obj: Any) = obj.isInstanceOf[Point] && obj.asInstanceOf[Point].x == y
+}
 
 object Demo {
   def main(args: Array[String]) {
-    var myList1 = range(10, 20, 2)
-    var myList2 = range(10,20)
+    val p1 = new Point(2, 3)
+    val p2 = new Point(2, 4)
+    val p3 = new Point(3, 3)
 
-    // Print all the array elements
-    for ( x <- myList1 ) {
-      print( " " + x )
-    }
-
-    println()
-    for ( x <- myList2 ) {
-      print( " " + x )
-    }
+    println(p1.isNotEqual(p2))
+    println(p1.isNotEqual(p3))
+    println(p1.isNotEqual(2))
   }
 }
